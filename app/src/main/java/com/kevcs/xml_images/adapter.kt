9def1f1239bf7ec359da3b5f914e9416c70bdf08
KevinCarm <kevincarm016@hotmail.com>
2021-02-27 20:14:20 -0600

    Quitanto import sin utilizar

--- app/src/main/java/com/kevcs/xml_images/adapter.kt
+++ app/src/main/java/com/kevcs/xml_images/adapter.kt
@@ -9,12 +9,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.toolbox.ImageLoader
 
 
 class adapter
